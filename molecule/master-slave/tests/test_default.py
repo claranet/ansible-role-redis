9a5e4d793e41ac56dc6eb5adad4d89012cb0129d
Bechir Delibes BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2023-01-13 13:54:41 +0100

    tests: master-slave scenario using one master and two slaves

--- molecule/master-slave/tests/test_default.py
+++ molecule/master-slave/tests/test_default.py
@@ -7,8 +7,3 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
-
-
-def test_shell(host):
-    command = host.run("sh --version")
-    assert command.rc == 0
