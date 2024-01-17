57cf867ebcbc6c75f819a097f3198b8124e0f38b
Antoine Ogereau <antoine.ogereau@fr.clara.net>
2024-01-17 09:28:31 +0100

    add molecule test for custom config and update readme

---
+++ molecule/custom-config/tests/test_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import stat
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+
+def test_file_config(host):
+    file = host.file("/etc/redis/redis_6379.conf")
+    assert file.exists
+    assert file.user == "redis"
+    assert file.contains("loglevel warning")
+
+
+def test_redis_listening(host):
+    assert host.socket("tcp://0.0.0.0:6379").is_listening
